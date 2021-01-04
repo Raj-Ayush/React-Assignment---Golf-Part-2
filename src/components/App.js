35c53053103e58ef7e08b6fed026fae6d350411d
Your name here <rajayush8571@gmail.com>
2021-01-04 19:19:45 +0530

    1st commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-import React, { Component, useState } from "react";
+import { render } from "enzyme";
+import React, { Component, useEffect, useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
@@ -9,8 +10,63 @@ const App = () => {
     left: "0px",
     top: "0px",
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+  
+  const reset = () => {
+    setRenderBall(false);
+    updateXY(0, 0);
+  };
+  const start = () =>{
+    setRenderBall(true);
+}
+  const renderChoice = () => {
+    if(renderBall){
+      return (
+      <div className="ball" style={{
+        position: "absolute",
+        left: ballPosition.left,
+        top:  ballPosition.top
+      }}></div>)
+    }
+    else{
+      return (
+      <button className="start" onClick={start}>
+        start
+      </button>
+      )
+    }
+  };
+
+  const updateXY = (newX, newY) => {
+    setX(newX);
+    setY(newY);
+    setBallPosition({
+      left: newX + 'px',
+      top: newY + 'px'
+    });
+  }
+  
+ 
+  useEffect(()=>{
+    const keyListener = (evt) => {
+      console.log("Listened to key");
+      if(renderBall){
+        if(evt.keyCode === 37){
+          updateXY(x-5, y);
+        } else if(evt.keyCode === 38){
+          updateXY(x, y-5);
+        } else if(evt.keyCode === 39){
+          updateXY(x+5, y);
+        } else if(evt.keyCode === 40){
+          updateXY(x, y+5);
+        }
+      } 
+      console.log(renderBall);
+    }
+    
+    document.addEventListener('keydown', keyListener);
+
+    return () => document.removeEventListener('keydown', keyListener);
+  }, [renderBall, x, y])
 
   return (
     <div className="playground">
